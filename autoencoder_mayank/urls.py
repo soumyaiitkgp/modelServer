45629633fda3a9b73593a2983402c8042362888b
mayanksingh1998 <smayank.singh.1998@gmail.com>
2020-06-22 19:21:13 +0530

    updated_mayank

--- autoencoder_mayank/urls.py
+++ autoencoder_mayank/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 # from rest_framework.authtoken import views
-from auto.views import *
+from autoencoder_mayank.views import *
 
 app_name = 'autoencoder_mayank'
 
