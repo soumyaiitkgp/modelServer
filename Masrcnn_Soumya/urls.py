31c2449f5f92860a6e0dbb9a053cda9eb00b42be
Curioussouya <psoumyadav@gmail.com>
2020-06-25 13:34:48 +0530

    Delete urls.py

--- Masrcnn_Soumya/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url, include
-from rest_framework.authtoken import views
-from captureImage.views import *
-
-app_name = 'Maskrcnn_Soumya'
-
-urlpatterns = [
-    url(r'predict', predict.as_view()),
-]
